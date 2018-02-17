a9fbb02cf778e5c4743ceab3b44bca39ff04e063
Vladimir Jimenez <allejo@me.com>
2018-02-17 14:05:48 -0800

    Add eyeglass-sassy-data

--- docs.sh
+++ docs.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
+TARDIR=./_site
+
+rm -rf projects/
+mkdir -p projects/{php,sass}/
+
+rm -rf _site/
+mkdir _site/
+
+#
+# PHP Projects
+#
+
 wget https://getcomposer.org/composer.phar --quiet
 chmod +x composer.phar
 
 ./composer.phar install --quiet
 
 SAMI=./vendor/bin/sami.php
-TARDIR=./_site
-
-# Check out our projects
-rm -rf projects/
-mkdir -p projects/php/
 
 git clone https://github.com/allejo/PhpPulse.git projects/php/PhpPulse
 git clone https://github.com/allejo/PhpSoda.git projects/php/PhpSoda
 git clone https://github.com/allejo/PhpWufoo.git projects/php/PhpWufoo
-
-rm -rf _site
-mkdir _site
 
 for f in ./projects/php/*
 do
@@ -25,6 +29,19 @@ do
 
     echo "Copying $f/docs/api/build/ to ../_site/"
     cp -af "$f/docs/api/build/." $TARDIR
+done
+
+#
+# Sass Projects
+#
+
+npm install
+
+git clone https://github.com/allejo/eyeglass-sassy-data.git projects/sass/eyeglass-sassy-data
+
+for f in ./projects/sass/*
+do
+    node_modules/.bin/sassdoc "$f/sass/*.scss" --dest "$TARDIR/$(basename $f)"
 done
 
 cp _redirects _site/_redirects
